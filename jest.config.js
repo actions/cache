50eb90a50338c9ff706348d12ad08ddad4ae6fc4
Josh Gross <jogros@microsoft.com>
2019-10-31 14:02:15 -0400

    Prevent commands from executing during tests

--- jest.config.js
+++ jest.config.js
@@ -9,3 +9,14 @@ module.exports = {
   },
   verbose: true
 }
+
+const processStdoutWrite = process.stdout.write.bind(process.stdout)
+process.stdout.write = (str, encoding, cb) => {
+  // We don't want :: commands to be executed by the runner during tests
+  // Replace any :: with :
+  if (!str.match(/^::/)) {
+    return processStdoutWrite(str, encoding, cb);
+  } else {
+    return processStdoutWrite(str.replace(/::/g, ":"), encoding, cb);
+  }
+}
