526c940a9430e8a83e7d42a409eb5cd04cc69d3b
Josh Gross <joshmgross@github.com>
2019-10-31 15:05:59 -0400

    Prevent commands from executing during tests (#21)
    * Prevent commands from executing during tests
    
    * Add newline at end of file
    
    * Drop all commands from output

--- jest.config.js
+++ jest.config.js
@@ -9,3 +9,12 @@ module.exports = {
   },
   verbose: true
 }
+
+const processStdoutWrite = process.stdout.write.bind(process.stdout)
+process.stdout.write = (str, encoding, cb) => {
+  // Core library will directly call process.stdout.write for commands
+  // We don't want :: commands to be executed by the runner during tests
+  if (!str.match(/^::/)) {
+    return processStdoutWrite(str, encoding, cb);
+  }
+}
