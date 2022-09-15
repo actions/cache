938abf1ecf95af94287621eec250465d2be81281
Shubham Tiwari <64764738+tiwarishub@users.noreply.github.com>
2022-09-15 08:30:00 +0000

    review comment

--- src/utils/actionUtils.ts
+++ src/utils/actionUtils.ts
@@ -82,7 +82,7 @@ export function isCacheFeatureAvailable(): boolean {
         if (isGhes()) {
             logWarning(
                 `Cache action is only supported on GHES version >= 3.5. If you are on version >=3.5 Please check with GHES admin if Actions cache service is enabled or not.
-Otherwise please upgrade to GHES version >= 3.5 and If you are also using Github Connect, please unretire the actions/cache namespace before upgradeSee https://docs.github.com/en/enterprise-server@3.5/admin/github-actions/managing-access-to-actions-from-githubcom/enabling-automatic-access-to-githubcom-actions-using-github-connect#automatic-retirement-of-namespaces-for-actions-accessed-on-githubcom`
+Otherwise please upgrade to GHES version >= 3.5 and If you are also using Github Connect, please unretire the actions/cache namespace before upgrade (see https://docs.github.com/en/enterprise-server@3.5/admin/github-actions/managing-access-to-actions-from-githubcom/enabling-automatic-access-to-githubcom-actions-using-github-connect#automatic-retirement-of-namespaces-for-actions-accessed-on-githubcom)`
             );
         } else {
             logWarning(
