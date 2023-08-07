a29b2aba121b0442a446aeb0fb071cd58d5e2dc4
Chad Kimes <1936066+chkimes@users.noreply.github.com>
2023-08-07 18:32:07 +0000

    Consume latest toolkit and fix dangling promise bug

--- src/restoreOnly.ts
+++ src/restoreOnly.ts
@@ -2,7 +2,19 @@ import restoreImpl from "./restoreImpl";
 import { NullStateProvider } from "./stateProvider";
 
 async function run(): Promise<void> {
-    await restoreImpl(new NullStateProvider());
+    try {
+        await restoreImpl(new NullStateProvider());
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    // node will stay alive if any promises are not resolved,
+    // which is a possibility if HTTP requests are dangling
+    // due to retries or timeouts. We know that if we got here
+    // that all promises that we care about have successfully
+    // resolved, so simply exit with success.
+    process.exit(0);
 }
 
 run();
