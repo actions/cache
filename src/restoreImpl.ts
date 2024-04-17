4c96456d462d1e756d5c6e25231ace11e8cba025
Matthew Matl <matthewcmatl@gmail.com>
2024-04-17 03:44:01 -0700

    feat: switch to @ambi-robotics/cache backend

--- src/restoreImpl.ts
+++ src/restoreImpl.ts
@@ -1,4 +1,4 @@
-import * as cache from "@actions/cache";
+import * as cache from "@ambi-robotics/cache";
 import * as core from "@actions/core";
 
 import { Events, Inputs, Outputs, State } from "./constants";
--- src/utils/actionUtils.ts
+++ src/utils/actionUtils.ts
@@ -1,4 +1,4 @@
-import * as cache from "@actions/cache";
+import * as cache from "@ambi-robotics/cache";
 import * as core from "@actions/core";
 
 import { RefKey } from "../constants";
@@ -64,17 +64,5 @@ export function isCacheFeatureAvailable(): boolean {
     if (cache.isFeatureAvailable()) {
         return true;
     }
-
-    if (isGhes()) {
-        logWarning(
-            `Cache action is only supported on GHES version >= 3.5. If you are on version >=3.5 Please check with GHES admin if Actions cache service is enabled or not.
-Otherwise please upgrade to GHES version >= 3.5 and If you are also using Github Connect, please unretire the actions/cache namespace before upgrade (see https://docs.github.com/en/enterprise-server@3.5/admin/github-actions/managing-access-to-actions-from-githubcom/enabling-automatic-access-to-githubcom-actions-using-github-connect#automatic-retirement-of-namespaces-for-actions-accessed-on-githubcom)`
-        );
-        return false;
-    }
-
-    logWarning(
-        "An internal error has occurred in cache backend. Please check https://www.githubstatus.com/ for any ongoing issue in actions."
-    );
     return false;
 }
