148157c8d68b3661fe1b148fbc3afd2b554f5ac6
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2024-05-16 16:10:51 +0300

    fix: Check for explicit true string

--- src/custom/backend.ts
+++ src/custom/backend.ts
@@ -40,10 +40,9 @@ const region =
     process.env.RUNS_ON_AWS_REGION ||
     process.env.AWS_REGION ||
     process.env.AWS_DEFAULT_REGION;
-const forcePathStyle = Boolean(
-    process.env.RUNS_ON_S3_FORCE_PATH_STYLE ||
-    process.env.AWS_S3_FORCE_PATH_STYLE
-);
+const forcePathStyle =
+    process.env.RUNS_ON_S3_FORCE_PATH_STYLE === "true" ||
+    process.env.AWS_S3_FORCE_PATH_STYLE === "true";
 
 const uploadQueueSize = Number(process.env.UPLOAD_QUEUE_SIZE || "4");
 const uploadPartSize =
